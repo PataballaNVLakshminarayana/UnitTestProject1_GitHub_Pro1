ed03bd538bef9b8aac03811b463dcbf8ce2fa752
Pataballa N V Lakshminarayana <narayana9573944031@gmail.com>
2025-05-18 08:53:45 +0530

    Add project files.

---
+++ UnitTestProject1_GitHub_Pro1/GitHub_Pro1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace UnitTestProject1_GitHub_Pro1
+{
+    [TestClass]
+    public class GitHub_Pro1
+    {
+        [TestMethod]
+        public void WebdriverSetUp()
+        {
+            var _Options = new ChromeOptions();
+            _Options.AddArgument("--headless");
+            _Options.AddArgument("--sandbox");
+            _Options.AddArgument("--disable-dev-shm-usage");
+            //new DriverManager();
+            IWebDriver _Drivar = new ChromeDriver();
+            _Drivar.Navigate().GoToUrl("https://adactinhotelapp.com/");
+            _Drivar.Manage().Window.Maximize();
+            _Drivar.FindElement(By.XPath("//input[@id='username']")).SendKeys("nagalakshmin");
+            _Drivar.FindElement(By.XPath("//input[@id='password']")).SendKeys("l@kshmin");
+            _Drivar.FindElement(By.XPath("//input[@id='login']")).Click();
+            Thread.Sleep(10000);
+            _Drivar.Quit();
+        }
+    }
+}
